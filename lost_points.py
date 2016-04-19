f2ba5fb39bdf2aff64ea2a635c9772648d67abdd
steve lilley <steven.lilley@stfc.ac.uk>
2016-04-19 09:37:08 -0700

    initial commit two tools lost points and plot nps stats

---
+++ lost_points.py
@@ -0,0 +1,39 @@
+"""
+Reads to output file and gets a list of the co-ordinates of the lost particles
+"""
+
+import matplotlib
+import matplotlib.pyplot as plt
+import datetime
+
+
+def output_lost_points(path):
+    """ """
+    with open(path) as f:
+        lines = f.read().splitlines()
+    f.close()
+
+    x = []
+    y = []
+    z = []
+    out_list = []
+    out_list.append("x y z temp")
+
+    for l in lines:
+        if l[0:19] == " x,y,z coordinates:":
+            x.append(float(l[28:40]))
+            y.append(float(l[43:55]))
+            z.append(float(l[58:70]))
+
+    i = 0
+    while i < len(x):
+        out_list.append(str(x[i])+ " " + str(y[i])+ " " + str(z[i]) + " 1")
+        i = i + 1
+
+    ofile = open(path+".3d", 'w')
+    for l in out_list:
+        ofile.write("%s\n" % l)
+
+
+if __name__ == "__main__":
+    output_lost_points(sys.argv[1])
---
+++ mcnp_run_plot.py
@@ -0,0 +1,69 @@
+"""
+Reads to output dumped to screen and plots rendevous frequency etc
+"""
+
+import matplotlib
+import matplotlib.pyplot as plt
+import datetime
+
+
+path = "/mnt/hgfs/transfer/10423.log"
+
+def plot_nps_stats(path):
+    """ reads std out file of mcnp run and produces graphs vs nps
+        these are useful for identifing any long history issues
+        process - read file, extract data, plot graphs 
+    """
+    with open(path) as f:
+        lines = f.read().splitlines()
+    f.close()
+
+    ctm = []
+    nrn = []
+    nps = []
+    coll = []
+    time = []
+
+    for l in lines:
+        if l[0:4] == " ctm":
+            ctm.append(float(l[6:18]))
+            nrn.append(int(l[26:44]))
+        if l[0:5] == " dump":
+            coll.append(int(l[61:78]))
+        if l[0:22] == " master set rendezvous":
+            nps.append(int(l[28:40]))
+            t1 = datetime.datetime.strptime(l[66:83], '%m/%d/%y %H:%M:%S')
+            time.append(t1)
+
+    d2 = matplotlib.dates.date2num(time)
+    fmt = matplotlib.dates.DateFormatter('%d/%m  %H:%M')
+
+    fig = plt.figure()
+    ax = fig.add_subplot(221)
+    ax.xaxis.set_major_formatter(fmt)
+    ax.set_xlabel("time")
+    ax.set_ylabel("nps")
+    ax.plot(time, nps)
+
+    ax1 = fig.add_subplot(222)
+    ax1.set_xlabel("nps")
+    ax1.set_ylabel("ctm")
+    ax1.plot(nps, ctm[1:])
+
+    ax2 = fig.add_subplot(223)
+    ax2.set_xlabel("nps")
+    ax2.set_ylabel("nrn")
+    ax2.plot(nps, nrn[1:])
+
+    ax3 = fig.add_subplot(224)
+    ax3.set_xlabel("nps")
+    ax3.set_ylabel("coll")
+    ax3.plot(nps, coll[1:])
+
+    fig.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    plot_nps_stats(sys.argv[1])
+
