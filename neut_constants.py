a411ed8a718b5a8a2e38d06e8f00bbffd764cd7f
py1sl <steven.lilley@stfc.ac.uk>
2019-03-06 14:17:08 +0000

    added some approximate constants

--- neut_constants.py
+++ neut_constants.py
@@ -4,3 +4,18 @@
 def get_e():
     """charge on electron in coulombs"""
     return 1.60217662e-19
+
+
+def get_p_mass():
+    """mass of proton in g"""
+    return 1.672623e-24
+
+
+def get_n_mass():
+    """mass of neutron in g"""
+    return 1.674929e-24
+
+
+def get_e_mass():
+    """ mass of electron in g"""
+    return 9.10938e-28
