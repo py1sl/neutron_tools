ef5710d855fc3d37020fedc9fce49b73b940be51
py1sl <steven.lilley@stfc.ac.uk>
2019-03-25 10:23:28 +0000

    update ofile reduce to use utilities

--- ofile_reduce.py
+++ ofile_reduce.py
@@ -9,51 +9,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import argparse
-
-
-def get_lines(path):
-    with open(path) as f:
-        lines = f.read().splitlines()
-    f.close()
-    return lines
-
-
-def find_line(text, lines, num):
-    """finds a index of the line in lines where the text is present in
-       the first num characters
-    """
-    i = 0
-    imax = len(lines) -1
-    for l in lines:
-        if i == imax:
-            raise EOFError(text + " not found in file")
-        i = i + 1
-        if l[0:num] == text:
-            print("found " + text + " at line :" + str(i))
-            return i - 1
-
-def write_lines(path, lines):
-    f = open(path, 'w')
-    for l in lines:
-        f.write(l)
-        f.write("\n")
-    f.close()
+import neut_utilities as ut
 
 
 def reduce_ofile(infile, ofile):
     """ reduces mcnp output file to just the data from last rendevous"""
 
-    lines=get_lines(infile)
+    lines=ut.get_lines(infile)
     
     # reduce to only the final result set
     try:
-        term_line = find_line("      run terminated when", lines, 25)
+        term_line = ut.find_line("      run terminated when", lines, 25)
     except EOFError as e:
         print(repr(e))
         sys.exit()
 
     lines = lines[term_line:]
-    write_lines(ofile, lines)
+    ut.write_lines(ofile, lines)
 
 
 if __name__ == "__main__":
