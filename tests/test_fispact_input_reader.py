2a9705835abb5a85dc78d2cbccfdc5554980719d
py1sl <steven.lilley@stfc.ac.uk>
2019-03-27 15:54:00 +0000

    add template test files

---
+++ tests/test_fispact_input_reader.py
@@ -0,0 +1,11 @@
+import unittest
+import fispact_input_reader
+
+class version_test_case(unittest.TestCase):
+    """ test for reading the version of output file"""
+    
+    def test_is_version(self):
+       self.assertTrue(True)
+        
+if __name__ == '__main__':
+    unittest.main()
