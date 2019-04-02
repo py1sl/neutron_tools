b372a577c85397eb4d23f32049f234ac7c26cde6
py1sl <steven.lilley@stfc.ac.uk>
2019-04-02 13:02:10 +0100

    added mat and tally num functions and tests

--- mcnp_input_reader.py
+++ mcnp_input_reader.py
@@ -8,9 +8,60 @@ import sys
 import argparse
 import neut_utilities as ut
 
+class mcnp_cell():
+    """ """
+    def __init__(self):
+        self.number = ""
+        self.mat = ""
+        self.density = 0.0
+        self.imp_p = 1.0
+        self.imp_n = 1.0
+        self.geom = ""
+        self.surfaces = []
+        
+        
+def get_full_line_comments(lines):
+    """ """
+    comments = []
+    for l in lines:
+        if len(l) > 1 and l[0].lower() == "c" and l[1] == " ":
+            comments.append(l)
+    return comments
+    
+    
+def get_material_numbers(lines):
+    """ """
+    mat_nums = []
+    for l in lines:
+        if len(l) > 1 and l[0].lower() == "m" and l[1].isdigit():
+             l = ut.string_cleaner(l)
+             l = l.split(" ")[0]
+             mnum = l[1:] 
+             mat_nums.append(int(mnum))
+    return mat_nums
+   
+   
+def get_tally_numbers(lines):
+    """ """
+    tal_nums = []
+    for l in lines:
+        if len(l) > 1 and l[0].lower() == "f" and l[1].isdigit():
+             l = ut.string_cleaner(l)
+             l = l.split(" ")[0]
+             l = l.split(":")[0]
+             tnum = l[1:] 
+             tal_nums.append(int(tnum))
+    return tal_nums
+
+    
 def read_mcnp_input(fpath):
     """ """
+
     ifile = ut.get_lines(fpath)
+    
+    comments = get_full_line_comments(ifile) 
+    mat_nums = get_material_numbers(ifile)
+ 
     return ifile
 
 if __name__ == "__main__":
--- tests/test_mcnp_input_reader.py
+++ tests/test_mcnp_input_reader.py
@@ -1,11 +1,62 @@
 import unittest
 import mcnp_input_reader
 
-class version_test_case(unittest.TestCase):
-    """ test for reading the version of output file"""
+class cell_card_tests(unittest.TestCase):
+    """ test for reading the cells part of input file"""
     
     def test_is_version(self):
        self.assertTrue(True)
+  
+  
+class surface_card_tests(unittest.TestCase):
+    """ test for reading the surfaces part of input file"""
+    
+    def test_is_version(self):
+       self.assertTrue(True)
+
+       
+class data_card_tests(unittest.TestCase):
+    """ test for reading the data part of input file"""
+    
+    def test_get_mat_nums(self):
+       """ """
+       test_list = ["M1",
+                    "m10",
+                    "m ",
+                    "m",
+                    "a "]
+       mnums = mcnp_input_reader.get_material_numbers(test_list)
+       self.assertEqual(mnums[0], 1)  # test for lower case m
+       self.assertEqual(mnums[1], 10)  # test for upper case m
+       self.assertEqual(len(mnums), 2)  # check nothing else added 
+       
+    def test_get_tal_nums(self):
+       """ """
+       test_list = ["f1",
+                    "F10",
+                    "f ",
+                    "f",
+                    "a "]
+       mnums = mcnp_input_reader.get_tally_numbers(test_list)
+       self.assertEqual(mnums[0], 1)  # test for lower case m
+       self.assertEqual(mnums[1], 10)  # test for upper case m
+       self.assertEqual(len(mnums), 2)  # check nothing else added
+
+       
+class misc_tests(unittest.TestCase):
+    """ test for reading the misc part of input file"""
+    
+    def test_full_line_comment(self):
+       """ """
+       test_list = ["c cell cards",
+                    "C surface ecards",
+                    "c234",
+                    "comment",
+                    "c"]
+       comments = mcnp_input_reader.get_full_line_comments(test_list)
+       self.assertEqual(comments[0], test_list[0])  # test for lower case c
+       self.assertEqual(comments[1], test_list[1])  # test for upper case c
+       self.assertEqual(len(comments), 2)  # check nothing else added as comment
         
 if __name__ == '__main__':
     unittest.main()
