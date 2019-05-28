f4ff3418a3aa880ed08311dd6c4a76b321889744
py1sl <steven.lilley@stfc.ac.uk>
2019-05-28 13:41:24 +0100

    added initial simple geometry tests

--- tests/test_geom_util.py
+++ tests/test_geom_util.py
@@ -1,11 +1,58 @@
 import unittest
 import geom_utils
+import numpy as np
 
-class version_test_case(unittest.TestCase):
-    """ test for reading the version of output file"""
+class geom_perim_test_case(unittest.TestCase):
+    """ tests for perimeter functions"""
     
-    def test_is_version(self):
-       self.assertTrue(True)
+    def test_perim_is_zero(self):
+        self.assertTrue(geom_utils.perim_circle(0.0),0.0)
+       
+    def test_perim_1_is_2pi(self):
+        self.assertTrue(geom_utils.perim_circle(1.0),2*np.pi)
+        
+        
+class geom_area_test_case(unittest.TestCase):
+    """ test for area functions"""
+    
+    def test_area_is_zero(self):
+        self.assertTrue(geom_utils.area_circle(0.0),0.0)
+        self.assertTrue(geom_utils.area_cyl(0.0, 1.0),0.0)
+        self.assertTrue(geom_utils.area_cyl(1.0, 0.0),0.0)
+        self.assertTrue(geom_utils.area_sphere(0.0),0.0)
+        self.assertTrue(geom_utils.area_cone_surf(0.0, 1.0),0.0)
+        self.assertTrue(geom_utils.area_cone_surf(1.0, 0.0),0.0)
+
+        
+class geom_volume_test_case(unittest.TestCase):
+    """ test for volume functions"""
+    
+    def test_vol_is_zero(self):
+        self.assertTrue(geom_utils.volume_sphere(0.0),0.0)
+        self.assertTrue(geom_utils.volume_cyl(0.0, 0.0),0.0)
+        self.assertTrue(geom_utils.volume_cyl(1.0, 0.0),0.0)
+        self.assertTrue(geom_utils.volume_cyl(0.0, 1.0),0.0)
+        self.assertTrue(geom_utils.volume_spherical_shell(0.0, 0.0),0.0)
+        self.assertTrue(geom_utils.volume_cyl_shell(0.0, 0.0, 0.0),0.0)
+        self.assertTrue(geom_utils.volume_cyl_shell(1.0, 1.0, 0.0),0.0)
+        self.assertTrue(geom_utils.volume_cone(0.0, 0.0),0.0)
+        self.assertTrue(geom_utils.volume_cone(1.0, 0.0),0.0)
+        self.assertTrue(geom_utils.volume_cone(0.0, 1.0),0.0)
+        
+        
+class geom_planes_test_case(unittest.TestCase):
+    """ tests for plane functions"""
+    
+    def test_dist_is_zero(self):
+        self.assertTrue(geom_utils.dist_between_planes(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),0.0)
+       
+        
+class geom_points_test_case(unittest.TestCase):
+    """ tests for points functions"""
+    
+    def test_dist_is_zero(self):
+        self.assertTrue(geom_utils.dist_bet_points(0.0, 0.0, 0.0, 0.0, 0.0, 0.0),0.0)      
+        
         
 if __name__ == '__main__':
     unittest.main()
