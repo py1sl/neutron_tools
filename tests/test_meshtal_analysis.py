9e439ce1632075fddbb44dbe0b212d0be86f8688
Olivia Tindle <69325996+OliviaT18@users.noreply.github.com>
2020-09-29 15:53:09 +0100

    Update test_meshtal_analysis.py

--- tests/test_meshtal_analysis.py
+++ tests/test_meshtal_analysis.py
@@ -6,3 +6,16 @@ Created on Tue Sep 29 12:58:31 2020
 @author: oliviatindle
 """
 
+import unittest
+
+class count_zeros_test(unittest.TestCase):
+    
+    def test_count_zeros(self):
+        test_zeros_data =[0, 1, 5, 8, 0, 3]
+        
+        self.assertEqual(count_zeros(test_zeros_data), 2)
+        
+class calc_midpoints_test(unittest.TestCase):
+    
+    def test_calc_midpoints(self):
+        test_midpoint_data =
