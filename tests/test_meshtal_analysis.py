bc602ebb8066c0b6db49d28dbd0f140dabab03b2
Olivia Tindle <69325996+OliviaT18@users.noreply.github.com>
2020-09-30 12:26:18 +0100

    Update test_meshtal_analysis.py

--- tests/test_meshtal_analysis.py
+++ tests/test_meshtal_analysis.py
@@ -7,21 +7,21 @@ Created on Tue Sep 29 12:58:31 2020
 """
 import meshtal_analysis
 import unittest
-import numpy as np
+
 
 class count_zeros_test(unittest.TestCase):
     
     def test_count_zeros(self):
         test_zeros_data =[0, 1, 5, 8, 0, 3]
         
-        self.assertEqual(count_zeros(test_zeros_data), 2)
+        self.assertEqual(meshtal_analysis.count_zeros(test_zeros_data), 2)
         
 class calc_mid_points_test(unittest.TestCase):
     
     def test_calc_mid_points(self):
-        test_midpoint_data = np.array[1, 9]
+        test_midpoint_data = [1, 9]
         
-        self.assertEqual(calc_mid_points(test_midpoint_data), 5)
+        self.assertEqual(meshtal_analysis.calc_mid_points(test_midpoint_data), 5)
         
 
 if __name__ == '__main__':
