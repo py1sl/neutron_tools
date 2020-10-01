ae08079bed476ddfb9ebf4fe8943117ed3073a1f
Olivia Tindle <69325996+OliviaT18@users.noreply.github.com>
2020-10-01 16:29:20 +0100

    Update test_meshtal_analysis.py

--- tests/test_meshtal_analysis.py
+++ tests/test_meshtal_analysis.py
@@ -8,22 +8,26 @@ Created on Tue Sep 29 12:58:31 2020
 import meshtal_analysis
 import unittest
 
-
-class count_zeros_test(unittest.TestCase):
     
-    def test_count_zeros(self):
-        test_zeros_data =[0, 1, 5, 8, 0, 3]
-        
-        self.assertEqual(meshtal_analysis.count_zeros(test_zeros_data), 2)
-        
-class calc_mid_points_test(unittest.TestCase):
+class calc_mid_points_test(unittest.TestCase):  'i think this is fine now'
     
     def test_calc_mid_points(self):
         test_midpoint_data = [1.0, 9.0]
         
-        self.assertEqual(meshtal_analysis.calc_mid_points(test_midpoint_data), [5.0])
+        self.assertEqual(meshtal_analysis.calc_mid_points(test_midpoint_data), [5.0])           
         
-        
+
+class count_zeros_test(unittest.TestCase):
+    
+    meshtally_test = meshtal_analysis.meshtally()
+    meshtally_test.data = [0, 1, 5, 3, 6, 7, 2, 0, 1, 8, 9]   'data is in the wrong format'
+    
+    def test_count_zeros(self):
+            
+         self.assertEqual(meshtal_analysis.count_zeros(meshtally_test.data, 2)
+  
+                          
+   
 #to do       
 #class find_mesh_tally_no_test(unittest.TestCase):
     
@@ -34,8 +38,8 @@ class calc_mid_points_test(unittest.TestCase):
         
 #class read_mesh_file_test(unittest.TestCase):
     #def test_read_mesh_file(self):   
-
-if __name__ == '__main__':
+        
+if __name__ == '__main__':      'god knows whats going on here'
     unittest.main()
-    
+
     
