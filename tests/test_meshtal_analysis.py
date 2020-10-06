67e945405de3073a0ba13eab7b5658ff6f564ea4
Olivia Tindle <69325996+OliviaT18@users.noreply.github.com>
2020-10-06 10:44:28 +0100

    Update test_meshtal_analysis.py

--- tests/test_meshtal_analysis.py
+++ tests/test_meshtal_analysis.py
@@ -9,37 +9,49 @@ import meshtal_analysis
 import unittest
 
     
-class calc_mid_points_test(unittest.TestCase):  'i think this is fine now'
+class calc_mid_points_test(unittest.TestCase):
     
     def test_calc_mid_points(self):
-        test_midpoint_data = [1.0, 9.0]
-        
-        self.assertEqual(meshtal_analysis.calc_mid_points(test_midpoint_data), [5.0])           
+    
+        self.assertEqual(meshtal_analysis.calc_mid_points(1.0, 9.0), 5.0)
+        self.assertEqual(meshtal_analysis.calc_mid_points(0.0, 1.0), 0.5) 
+        self.assertEqual(meshtal_analysis.calc_mid_points(1.0, 1.5), 1.25)          
         
 
 class count_zeros_test(unittest.TestCase):
     
     meshtally_test = meshtal_analysis.meshtally()
-    meshtally_test.data = [0, 1, 5, 3, 6, 7, 2, 0, 1, 8, 9]   'data is in the wrong format'
-    
+    meshtally_test.data = ['0.00', '5.00', '3.00', '6.00', '-1.00', '0.00', '8.00']
+   
     def test_count_zeros(self):
             
          self.assertEqual(meshtal_analysis.count_zeros(meshtally_test.data, 2)
   
-                          
+
+if __name__ == '__main__':
+    unittest.main()
+    
+    
+
+
+
+                        
    
 #to do       
 #class find_mesh_tally_no_test(unittest.TestCase):
     
     #def test_find_mest_tally_no(self):
 
-#class read_mesh_test(unittest.TestCase):
+#class read_mesh_test(unittest.TestCase):     
     #def test_read_mest(self):
+#think this is the one to do next - apparently just give path to the function' 
+#(still not too sure how to do that)'
+#should then give list of mesh objects so can compare and see if data matches'
+#in theory anyway'
         
 #class read_mesh_file_test(unittest.TestCase):
     #def test_read_mesh_file(self):   
         
-if __name__ == '__main__':      'god knows whats going on here'
-    unittest.main()
+
 
     
