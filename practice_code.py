db41cadc9b051c50823d7430707045f3dff5be62
Olivia Tindle <69325996+OliviaT18@users.noreply.github.com>
2020-11-02 16:48:48 +0000

    Update practice_code.py

--- practice_code.py
+++ practice_code.py
@@ -5,7 +5,7 @@ Practice so i can see things clearer
 
 """
 #To Do:
-#Rough idea - tell steve
+#Rough idea
 #pray
 #dont forget print statements 
 
@@ -18,8 +18,8 @@ meshes = meshtal_analysis.read_mesh_tally_file("/Users/oliviatindle/Desktop/Plac
 df = meshes[0].data
 print(df)      #this is a dataframe
 
-#data = df.rel_err.to_string(index = False) 
-#df.hist(data, bins = 15)
+data = df.rel_err.to_string(index = False) 
+df.hist(data, bins = 15)
 
 
 data = [df.rel_err]
