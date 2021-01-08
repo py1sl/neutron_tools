d81bcbaa080edd39e98b53953313fa41e040539a
py1sl <steven.lilley@stfc.ac.uk>
2021-01-08 08:53:01 +0000

    set up files for fispact input testing

--- tests/test_fispact_input_reader.py
+++ tests/test_fispact_input_reader.py
@@ -1,23 +1,13 @@
 import unittest
 import fispact_input_reader
 
-class version_test_case(unittest.TestCase):
+class read_input_test_case(unittest.TestCase):
     """ test for reading the version of output file"""
     
-    def test_is_version(self):
-       
-       # test get_irrad_times
-       # self.assertEqual(len(),)
-       # test get_cooldown times
-       # self.assertEqual(len(),)
-       # test get material
-       # self.assertEqual(len(),)
-       # self.assertEqual(,)
-       # test get mass
-       # self.assertEqual(,)
-       # test get comments
-       # self.assertEqual(,)
-       self.assertEqual(1.0,1.0)
+    def test_read(self):
+       path = "test_output/fis_in.i"
+       self.assertEqual(len(fispact_input_reader.read_fispact_input(path)), 64)
+
         
 if __name__ == '__main__':
     unittest.main()
