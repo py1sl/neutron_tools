51032fb677bda90c7cdd479956bd73af0cf47401
py1sl <steven.lilley@stfc.ac.uk>
2021-02-04 21:52:40 +0000

    improved testing for mcnp analysis

--- tests/test_mcnp_analyis.py
+++ tests/test_mcnp_analyis.py
@@ -10,18 +10,21 @@ class normalise_test_case(unittest.TestCase):
         self.assertEqual(ma.normalise(data, 10), [10, 20, 30, 40])
 
 
-class calc_err_test(unittest.TestCase):
+class calc_absolute_err_test(unittest.TestCase):
     """ test for absolute error calculation function"""
 
-    def test_is_version(self):
-        self.assertTrue(True)
+    def test_abs_err_calc(self):
+        err = [1, 0.5, 0.1]
+        res = [1, 1, 1]
+        self.assertEqual(ma.calc_err_abs(res, err), [1, 0.5, 0.1])
 
 
 class calc_bin_width_test(unittest.TestCase):
     """ test for bin width calculation function"""
 
-    def test_is_version(self):
-        self.assertTrue(True)
+    def test_bin_widths(self):
+        bins = [1, 1.5, 1.6]
+        self.assertEqual(len(ma.calc_bin_width(bins)), 3)
 
 
 if __name__ == '__main__':
