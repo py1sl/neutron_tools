417aa076e6f67a17c7d8ef43c9c531a765b8b28d
py1sl <steven.lilley@stfc.ac.uk>
2021-03-01 19:22:43 +0000

    start adding fispact output tests

--- tests/test_fispact_output_reader.py
+++ tests/test_fispact_output_reader.py
@@ -1,13 +1,31 @@
 import unittest
 import fispact_output_reader as fo
+import neut_utilities as ut
 
 
-class find_ind_test_case(unittest.TestCase):
+class version_test_case(unittest.TestCase):
     """ test for reading the version of output file"""
 
-    def test_find_ind(self):
-        self.assertEqual(1, 1)
+    def test_version(self):
+        path = "test_output/fis_test1.out"
+        lines = ut.get_lines(path)
+        version = fo.check_fisp_version(lines)
+        self.assertEqual(version, "FISPACT-II")
 
+    def test_fis2version(self):
+        path = "test_output/fis_test1.out"
+        lines = ut.get_lines(path)
+        version = fo.isFisII(lines)
+        self.assertEqual(version, True)
+
+        
+class read_fis_out_test_case(unittest.TestCase):
+    """ test for the read fis out function which tests many other functions """
+    
+    def test_fis_out_file1(self):
+        path = "test_output/fis_test1.out"
+        output = fo.read_fis_out(path)
+        self.assertEqual(output.file_name, path)
 
 if __name__ == '__main__':
     unittest.main()
