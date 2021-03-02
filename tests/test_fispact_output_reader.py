76f635a84edf9cbb9604923cfc50729c890d52c2
py1sl <steven.lilley@stfc.ac.uk>
2021-03-02 08:09:13 +0000

    more fispact output reader tests

--- tests/test_fispact_output_reader.py
+++ tests/test_fispact_output_reader.py
@@ -26,6 +26,12 @@ class read_fis_out_test_case(unittest.TestCase):
         path = "test_output/fis_test1.out"
         output = fo.read_fis_out(path)
         self.assertEqual(output.file_name, path)
+        self.assertEqual(output.version, "FISPACT-II")
+        self.assertEqual(output.isFisII, True)
+        self.assertEqual(output.cpu_time, 8.2558)
+        self.assertEqual(output.tot_irrad_time, 1.2e3)
+        self.assertEqual(output.tot_fluence, 7.2e9)
+        self.assertEqual(output.ave_flux, 6.0e6)
 
 if __name__ == '__main__':
     unittest.main()
