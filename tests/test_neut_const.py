6f6b2c55e5dc7d9486b6219478f4263730a34457
Riaan-jansen <126263953+Riaan-jansen@users.noreply.github.com>
2023-08-16 16:57:08 +0100

    Update test_neut_const.py

--- tests/test_neut_const.py
+++ tests/test_neut_const.py
@@ -46,11 +46,11 @@ class Z_dict_test_case(unittest.TestCase):
     """ test for periodic table """
 
     def test_table_len(self):
-        self.assertEqual((len(nc.Z_dict()), 118)
-                         
-    def test_last_value(self):
+        self.assertEqual((len(nc.Z_dict())), 118)
+    
+    def test_max_value(self):
         z = nc.Z_dict()
-        self.assertEqual(z.get("Og"), 118)
+        self.assertEqual(max(z.values()), 118)
 
 if __name__ == '__main__':
     unittest.main()
