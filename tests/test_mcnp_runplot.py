32f1ac054c8a6f5430d435408228ba6241c184e3
py1sl <steven.lilley@stfc.ac.uk>
2024-01-26 08:54:46 +0000

    pep8

--- tests/test_mcnp_runplot.py
+++ tests/test_mcnp_runplot.py
@@ -5,11 +5,11 @@ import mcnp_run_plot as mrp
 
 class plotting_tests(unittest.TestCase):
     """ tests for plotting functions """
-   
+
     @patch("matplotlib.pyplot.savefig")
     @patch("matplotlib.pyplot.show")
     def test_run_plot(self, mock_show, mock_savefig):
-        
+
         test_path = "test_output/run_output"
         mrp.plot_nps_stats(test_path)
         mock_show.assert_called_once()
@@ -19,6 +19,7 @@ class plotting_tests(unittest.TestCase):
         mrp.plot_nps_stats(test_path, fname=fname)
         # Assert that savefig was called with the specified filename
         mock_savefig.assert_called_once_with(fname)
+
 
 if __name__ == '__main__':
     unittest.main()
