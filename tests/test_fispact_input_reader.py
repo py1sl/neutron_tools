3eea78ff23b2aac7087357279067c310bbaeb79a
Ella Beck <127053515+ellabeck03@users.noreply.github.com>
2024-07-17 11:56:19 +0100

    convert string path to path objects in tests (#44)
    Co-authored-by: Ella Beck <Ella.Beck@stfc.ac.uk>

--- tests/test_fispact_input_reader.py
+++ tests/test_fispact_input_reader.py
@@ -1,12 +1,13 @@
 import unittest
 import fispact_input_reader
+import os
 
 
 class read_input_test_case(unittest.TestCase):
     """ test for reading the version of output file"""
 
     def test_read(self):
-        path = "test_output/fis_in.i"
+        path = os.path.join(os.path.dirname(__file__), 'test_output', 'fis_in.i')
         self.assertEqual(len(fispact_input_reader.read_fispact_input(path)),
                          64)
 
