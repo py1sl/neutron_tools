4dccfd91684fec89cda58c4c952e16ac2b8e6559
py1sl <steven.lilley@stfc.ac.uk>
2024-09-25 09:05:46 +0100

    improving generic output functions

--- output_utilities.py
+++ output_utilities.py
@@ -1,34 +1,53 @@
 """
 uesful utilities for common output options
 """
+import csv
 import neut_utilities as ut
 
 
 def output_points(x_vals, y_vals, z_vals, data_val=None, outpath="lost"):
-    """ outputs points in .3d format """
+    """ Outputs points in .3d format """
 
-    out_list = []
-    out_list.append("x y z temp")
+    # Initialize output list with the header
+    out_list = ["x y z temp"]
 
-    i = 0
+    # Generate points with associated data or default value '1'
     if data_val is not None:
-        while i < len(x_vals):
-            out_list.append(str(x_vals[i]) +
-                            " " +
-                            str(y_vals[i]) +
-                            " " +
-                            str(z_vals[i]) +
-                            str(data_val[i]))
-            i = i + 1
+        out_list.extend(
+            f"{x} {y} {z} {d}" for x, y, z, d in zip(x_vals, y_vals, z_vals, data_val)
+        )
     else:
-        while i < len(x_vals):
-            out_list.append(str(x_vals[i]) +
-                            " " +
-                            str(y_vals[i]) +
-                            " " +
-                            str(z_vals[i]) +
-                            " 1")
-            i = i + 1
+        out_list.extend(
+            f"{x} {y} {z} 1" for x, y, z in zip(x_vals, y_vals, z_vals)
+        )
 
-    outpath = outpath + ".3d"
+    # Append the file extension
+    outpath = f"{outpath}.3d"
+
+    # Write the list of points to the file
     ut.write_lines(outpath, out_list)
+
+
+def output_csv(data, outpath="output.csv"):
+    """Outputs a list of lists as a CSV file.
+
+    """
+    with open(outpath, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
+
+        
+def list_to_html_table(data):
+    """Converts a list of lists into an HTML table.
+
+    """
+    html = "<table border='1'>\n"
+    
+    for row in data:
+        html += "  <tr>\n"
+        for item in row:
+            html += f"    <td>{item}</td>\n"
+        html += "  </tr>\n"
+    
+    html += "</table>"
+    return html
